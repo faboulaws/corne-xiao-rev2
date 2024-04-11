34a1e77b72c265bdaf97cd91fe76eba854aeae6e
Lawson, Lod <Lod.Lawson@netapp.com>
2024-04-11 12:50:37 +0300

    update commit message

--- copy-and-commit-keymap.sh
+++ copy-and-commit-keymap.sh
@@ -7,9 +7,11 @@ cp config/corny.keymap config/boards/shields/corny/corny.keymap
 if git diff --cached --name-only | grep -q "config/boards/shields/corny/corny.keymap" || git diff --name-only | grep -q "config/boards/shields/corny/corny.keymap"; then
   # Add and commit original file (if changes exist)
   git add config/boards/shields/corny/corny.keymap
-  # Get the last commit message (excluding first line)
-  last_commit_message=$(git log -1 --pretty=%B | sed 1d)
-  git commit -m "$last_commit_message Updated config/boards/shields/corny/corny.keymap from config/corny"
+  latest_commit_sha=$(git rev-parse HEAD)
+  # Use the SHA to access the message elsewhere in your script (if needed)
+  last_commit_message=$(git log --format=%B -n 1 $latest_commit_sha)
+  
+  git commit -m "$last_commit_message Updated config/boards/shields/corny/corny.keymap from config/corny.keymap changes."
   echo "Committed changes to config/boards/shields/corny/corny.keymap"
   git push
 else
